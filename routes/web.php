ab2533842dce7b635b59e8335ce737c7906d8d81
zidanf <zidanfadillah006@gmail.com>
2025-09-19 22:00:10 +0700

    odading

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\cartController;
+use App\Http\Controllers\checkoutController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +14,6 @@ Route::get('/dashboard', function () {
 });
 
 
-// Route::middleware('auth')->group(function(){
-//     Route::resource('product', )
-//  });
+Route::resource('/product', App\Http\Controllers\ProductController::class);
+Route::resource('/cart', App\Http\Controllers\cartController::class);
+Route::resource('/checkout', App\Http\Controllers\checkoutController::class);
